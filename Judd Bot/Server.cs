f4e9a1094703376ddc1102d9c1d3600c5b4372c6
Jasper Gray <professorchem1@gmail.com>
2020-06-07 23:21:14 +0100

    Update Server.cs

--- Judd Bot/Server.cs
+++ Judd Bot/Server.cs
@@ -1,91 +1,48 @@
-﻿/*using System;
+﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Reflection;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using Judd_Bot;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace Judd_Bot
 {
     class Server
     {
-        TcpListener server = null;
-        public Server(int port)
+        public static async Task SQLQuery(string usertofind)
         {
-            IPAddress localAddr = IPAddress.Any;
-            server = new TcpListener(localAddr, port);
-            server.Start();
-            StartListener(localAddr.ToString());
-        }
-        public void StartListener(string ip)
-        {
+            var dbinfo = File.ReadAllText(@"sqlinfo.txt");
+            MySqlConnection conn = new MySqlConnection(dbinfo);
             try
             {
-                while (true)
-                {
-                    Console.WriteLine("\nWaiting for a connection\n");
-                    TcpClient client = server.AcceptTcpClient();
-                    Console.WriteLine("Connected!");
-                    Thread t = new Thread(new ParameterizedThreadStart(HandleDevice));
-                    t.Start(client);
-                }
-            }
-            catch (SocketException e)
-            {
-                Console.WriteLine("SocketException: {0}", e);
-                server.Stop();
-            }
-        }
-        public void HandleDevice(Object obj)
-        {
-            TcpClient client = (TcpClient)obj;
-            var stream = client.GetStream();
-            string imei = String.Empty;
-            string data = null;
-            Byte[] bytes = new Byte[256];
-            int i;
-            try
-            {
-                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
-                {
-                    string hex = BitConverter.ToString(bytes);
-                    data = Encoding.ASCII.GetString(bytes, 0, i);
-                    Console.WriteLine("{1}: Received: {0}", data, Thread.CurrentThread.ManagedThreadId);
-                    if (data.StartsWith("[DATA]"))
-                    {
-                        string str = "Data Recieved: ";
-                        Byte[] reply = System.Text.Encoding.ASCII.GetBytes(str + data.Remove(0, 7) + "\n");
-                        stream.Write(reply, 0, reply.Length);
-                        Console.WriteLine("{1}: Response Returned: {0}", str + data.Remove(0, 7), Thread.CurrentThread.ManagedThreadId);
-                        var todo = data.Remove(0, 7);
-                        var splitted = todo.Split(' ');
-                        foreach (var elem in splitted)
-                        {
-                            Console.WriteLine(elem);
-                        }
-                        AdministrationCommands admin = new AdministrationCommands();
-                        admin.Assign(splitted[0], splitted[1]);
-                    }
-                    else
-                    {
-                        string str = "Invalid Request\n";
-                        Byte[] reply = System.Text.Encoding.ASCII.GetBytes(str);
-                        stream.Write(reply, 0, reply.Length);
-                        Console.WriteLine("{1}: Response Returned: {0}", str, Thread.CurrentThread.ManagedThreadId);
-                    }
+                Console.WriteLine("Connecting to Database...");
+                conn.Open();
 
+                string sql = "SELECT Name, HeadOfState FROM Country WHERE Continent='Oceania'";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Console.WriteLine(rdr[0] + " -- " + rdr[1]);
                 }
+                rdr.Close();
             }
-            catch (Exception e)
+            catch (Exception ex)
             {
-                Console.WriteLine("Exception: {0}", e.ToString());
-                client.Close();
+                Console.WriteLine(ex.ToString());
             }
+            conn.Close();
+            Console.WriteLine("SQL query complete.");
         }
     }
 }
 
-    */
+    
